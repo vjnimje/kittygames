332bc33ff01c4599b211128f4d52eb3314d3c501
Vijay Nimje <vjnimje@yahoo.com>
2021-10-21 09:47:13 +0530

    mobile responsive

--- application/views/header.php
+++ application/views/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Kitty Games</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/blur.css">
