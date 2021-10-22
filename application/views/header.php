71c648a21eef949d75d7b18ed3be508ef01e41a6
Vijay Nimje <vjnimje@yahoo.com>
2021-10-22 20:14:49 +0530

    Navigation Bar added

--- application/views/header.php
+++ application/views/header.php
@@ -1,18 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	   <!--<meta charset="utf-8"> -->
+	   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	   <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Kitty Games</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/blur.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/shapes.css">
 </head>
 <body>
-  	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  		<div class="container">
-  			<a class="navbar-brand" href="<?php echo base_url();?>">Kitty Games</a>
-  		</div>
-    	
-  	</nav>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd; margin-bottom: 2rem;">
+	<div class="container" style="padding:0.5rem;">
+		<a class="navbar-brand" href="<?php echo base_url();?>">Kitty Games</a>
+		    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			</button>
+		  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+		    <ul class="navbar-nav">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="<?php echo base_url();?>gameone">Round One</a>
+		      </li>
+		      <li class="nav-item active">
+		        <a class="nav-link" href="<?php echo base_url();?>gametwo">Round Two</a>
+		      </li>
+		      <li class="nav-item active">
+		        <a class="nav-link" href="<?php echo base_url();?>gamethree">Round Three</a>
+		      </li>
+		    </ul>
+		</div>
+	</div>
+</nav>
   	
